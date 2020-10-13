6b97a1d0652554457d96bf745cc7fb36341757ea
AmitaiF <57657186+AmitaiF@users.noreply.github.com>
2020-10-13 13:09:01 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 chrome.browserAction.onClicked.addListener(function(activeTab){
-  var newURL = "http://stackoverflow.com/";
   chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     let url = tabs[0].url;
     // use `url` here inside the callback because it's asynchronous!
